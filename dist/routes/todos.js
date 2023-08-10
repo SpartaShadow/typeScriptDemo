d859970fb78d720171e3db07a1d3f8a4c7048d0a
SpartaShadow <114387187+SpartaShadow@users.noreply.github.com>
2023-08-11 02:06:48 +0530

    Typescript showdown

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -7,24 +7,28 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todos.push(newTodo);
     res.status(200).send({ res: todos, message: "success" });
 });
 router.put('/put/:todoid', (req, res, next) => {
-    const id = req.params.todoid;
+    const param = req.params;
+    const id = param.todoid;
+    const body = req.body;
     const index = todos.findIndex(ele => ele.id === id);
     if (index >= 0) {
-        todos[index] = { id: todos[index].id, text: req.body.text };
+        todos[index] = { id: todos[index].id, text: body.text };
         return res.status(201).json({ res: todos, message: 'put success' });
     }
     res.status(404).json({ message: 'could not found' });
 });
 router.delete('/delete/:todoid', (req, res, next) => {
-    const id = req.params.todoid;
+    const param = req.params;
+    const id = param.todoid;
     todos = todos.filter(ele => ele.id != id);
     res.status(201).json({ res: todos, message: 'delete success' });
 });
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,38 +1,44 @@
 import {Router} from 'express';
-import {Todo} from '../model/todos';
+import {Todo} from '../model/todos'
 
 const router= Router();
 let todos: Todo[]= [];
+
+type bodyReq= { text: string};
+type paramReq= { todoid: string};
+
 router.get('/',(req,res,next)=>{
     res.status(200).json({todos:todos})
 })
 
 router.post('/todo',(req,res,next)=>{
+    const body=req.body as bodyReq;
     const newTodo: Todo ={
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     }
-    
     todos.push(newTodo);
     res.status(200).send({res:todos,message:"success"})
 })
 
 router.put('/put/:todoid',(req,res,next)=>{
-    const id=req.params.todoid;
-    const index=todos.findIndex(ele=> ele.id === id);
+    const param=req.params as paramReq;
+    const id=param.todoid;
+    const body=req.body as bodyReq;
+    const index=todos.findIndex(ele=> ele.id === id); 
     if(index>=0){
-        todos[index]= { id: todos[index].id, text:req.body.text};
+        todos[index]= { id: todos[index].id, text:body.text};
         return res.status(201).json({res:todos ,message:'put success'});
     }
     res.status(404).json({message:'could not found'});
 })
 
 router.delete('/delete/:todoid',(req,res,next)=>{
-    const id=req.params.todoid;
+    const param=req.params as paramReq;
+    const id=param.todoid;
     todos = todos.filter(ele=> ele.id!=id); 
     res.status(201).json({res:todos ,message:'delete success'});
 })
 
 //module.exports= router;
-
 export default router;
